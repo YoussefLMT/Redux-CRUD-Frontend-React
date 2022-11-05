eb478cda442b26d6b18ad7b0b6d3661fb839c733
YoussefLMT <mcyoussef324@gmail.com>
2022-11-05 19:37:04 +0100

    add add user modal

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
               Users Managment
 
               {/* Button trigger add user modal */}
-            <button type="button" class="btn btn-primary" style={{float: 'right'}} data-bs-toggle="modal" data-bs-target="#addUserModal">
+              <button type="button" class="btn btn-primary" style={{ float: 'right' }} data-bs-toggle="modal" data-bs-target="#addUserModal">
                 Add User
-            </button>
+              </button>
             </div>
           </div>
           <div class="card-body">
@@ -41,6 +41,38 @@ function App() {
                 </tr>
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+
+      {/* Add User Modal  */}
+      <div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="name" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="name" />
+                </div>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email" />
+                </div>
+                <div class="mb-3">
+                  <label for="passowrd" class="form-label">Password</label>
+                  <input type="passowrd" class="form-control" id="passowrd" />
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
