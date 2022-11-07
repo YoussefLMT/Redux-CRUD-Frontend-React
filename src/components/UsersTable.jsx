58ea76f92d8268bc4e659c5a0d17eb3dd0a12392
YoussefLMT <mcyoussef324@gmail.com>
2022-11-07 20:52:56 +0100

    add update user modal

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosInstance from '../axios';
 
 function UsersTable(props) {
@@ -30,33 +30,67 @@ function UsersTable(props) {
     }, [])
 
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.users.map((user) => {
-                        return (
-                            <tr key={user.id}>
-                                <th scope="row">{user.id}</th>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>
-                                    <button type="button" onClick={(e) => deleteUser(e, user.id)} class="btn btn-danger">Delete</button>
-                                    <button type="button" data-bs-toggle="modal" data-bs-target="#updateUserModal" onClick={() => getUser(user.id)} class="btn btn-warning">Update</button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+        <>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.users.map((user) => {
+                            return (
+                                <tr key={user.id}>
+                                    <th scope="row">{user.id}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <button type="button" onClick={(e) => deleteUser(e, user.id)} class="btn btn-danger">Delete</button>
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#updateUserModal" onClick={() => getUser(user.id)} class="btn btn-warning">Update</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+            {/* update user modal */}
+            <div className="modal fade" id="updateUserModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Update User</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input type="text" name='name' className="form-control" id="name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="model" className="form-label">Email</label>
+                                    <input type="text" name='model_year'  className="form-control" id="model" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="color" className="form-label">Password</label>
+                                    <input type="text" name='color' className="form-control" id="color" />
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-primary">Update</button>
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
