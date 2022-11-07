b911d18bc5e2a2eaad1313db9b4503bcb26996bb
YoussefLMT <mcyoussef324@gmail.com>
2022-11-07 18:50:39 +0100

    add getUsers

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,4 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axiosInstance from '../axios'
+
+export const getUsers = createAsyncThunk('users/getUsers', async () => {
+    try {
+        const response = await axiosInstance.get('/users')
+        return response.data.users
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 const userSlice = createSlice({
     name: "users",
