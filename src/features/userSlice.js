aa0e6a59b6b082fc229a683faa8810277a24b0c7
YoussefLMT <mcyoussef324@gmail.com>
2022-11-07 12:37:40 +0100

    remove addNewUser

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,7 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axiosInstance from '../axios';
 
-export const addNewUser = createAsyncThunk('users/addNewUser', async (data) => {
-    const response = await axiosInstance.post('/users', data)
-    console.log(response.data)
+const userSlice = createSlice({
+    name: "users",
+    initialState: {
+        users: [],
+        loading: false,
+        error: null
+    },
 })
