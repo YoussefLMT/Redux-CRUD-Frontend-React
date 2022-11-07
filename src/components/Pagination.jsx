985718055fc96e7751b8a6fbbb89a70f73b60e2e
YoussefLMT <mcyoussef324@gmail.com>
2022-11-07 21:44:41 +0100

    add pagination content

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-function Pagination() {
-  return (
-    <div>Pagination</div>
-  )
+function Pagination(props) {
+
+    const pageNumbers = []
+
+    for (let i = 1; i <= Math.ceil(props.totalusers / props.usersPerPage); i++) {
+        pageNumbers.push(i)
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {
+                    pageNumbers.map((number) => (
+                        <li key={number} className="page-item">
+                            <a className="page-link" onClick={() => props.paginate(number)} href="#">{number}</a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
 }
 
 export default Pagination
