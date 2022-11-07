91e8ebb96e5379ca67c9745dd58741a4ad2d0771
YoussefLMT <mcyoussef324@gmail.com>
2022-11-07 20:07:55 +0100

    add table

--- src/App.js
+++ src/App.js
@@ -73,30 +73,7 @@ function App() {
               </button>
             </div>
             <div className="card-body">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                </tbody>
-              </table>
+              
             </div>
           </div>
         </div>
--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 function UsersTable() {
-  return (
-    <div>UsersTable</div>
-  )
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+            </tbody>
+        </table>
+    )
 }
 
 export default UsersTable
